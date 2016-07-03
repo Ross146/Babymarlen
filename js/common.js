d6fa43a0545e94c65156f1b3d075bb44f650a9eb
y.padalka <y.padalka@umberfirm.com>
2016-07-03 22:40:38 +0300

    main-section

--- js/common.js
+++ js/common.js
@@ -3,10 +3,37 @@ $(document).ready(function(){
         items: 1,
         loop: true
     });
-    $('.brands-carousel-container').owlCarousel({
-        items: 6,
-        loop: true,
-        nav: true,
-        autoplay: true
+
+    function initBrandCarousel() {
+        var $owlCarousel = $('.brands-carousel-container');
+        var items = ($(window).width() < 900)? 5 : ($(window).width() < 1260)? 4: 6;
+        function initCarousel() {
+            $owlCarousel.owlCarousel({
+                items: items,
+                dots:false,
+                loop: true,
+                nav: true,
+                autoplay: true
+            });
+        }
+
+        function destroyCarousel() {
+            $owlCarousel.trigger('destroy.owl.carousel');
+            $owlCarousel.html($owlCarousel.find('.owl-stage-outer').html()).removeClass('owl-loaded');
+        }
+
+        function refreshCarousel() {
+            destroyCarousel();
+            initCarousel();
+        }
+
+        refreshCarousel();
+    }
+
+    $(window).resize(function() {
+        initBrandCarousel();
     });
+
+    initBrandCarousel();
+
 });
