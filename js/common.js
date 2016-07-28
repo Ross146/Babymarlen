a314538445e0cdde1158c2ee12e7961f0148ba0f
y.padalka <y.padalka@umberfirm.com>
2016-07-28 17:26:50 +0300

    shit shit shit

--- js/common.js
+++ js/common.js
@@ -1,8 +1,5 @@
 (function(){
     function gallerySlide($gallerySlide) {
-
-
-
         if ($(window).width() > 500 && !!$gallerySlide.find(('.owl-stage-outer').length)){
             $gallerySlide.trigger('destroy.owl.carousel');
             $gallerySlide.html($gallerySlide.find('.owl-stage-outer').html()).removeClass('owl-loaded');
@@ -18,19 +15,48 @@
 
     }
 
-    $(document).ready(function(){
-        $('.section-slider').owlCarousel({
-            items: 1,
-            loop: true
-        });
 
-        gallerySlide($('.instagram-gallery .photo-container'));
 
-        $(window).resize(function () {
-            gallerySlide($('.instagram-gallery .photo-container'));
+    function toogleDesignersTabs () {
+        ($(window).width() <=500)? $('.designers .tab').removeClass('active'): $('.designers .tab:first-child').addClass('active').siblings().removeClass('active') ;
+        $('.designers .tab .head').click(function(){
+            var $this = $(this);
+            if($(window).width() <= 500 && $this.parent().hasClass('active')){
+                $this.parent().removeClass('active');
+            } else{
+                $this.parent().addClass('active').siblings().removeClass('active');
+            }
         })
+    }
+
+    function calcDesignersTablesColums () {
+        $('.designers .tab').each(function () {
+            var count = 0;
+            var $items = $(this).find('.tab-content.desktop li');
+            var $tableContainerUl = $(this).find('.tab-content.tablet ul');
+            var $numberColumn = Math.ceil($(this).find('li').length/4);
+            for(var i =0; i< $tableContainerUl.length; i ++){
+                for(var j = 0 ; j< $numberColumn; j++) {
+                    $tableContainerUl.eq(i).append($items.eq(count++).clone())
+                }
+            }
+        });
+    };
+
+    $('.section-slider').owlCarousel({
+        items: 1,
+        loop: true
     });
 
+    gallerySlide($('.instagram-gallery .photo-container'));
 
+
+    toogleDesignersTabs();
+    calcDesignersTablesColums();
+
+    $(window).resize(function () {
+        gallerySlide($('.instagram-gallery .photo-container'));
+        toogleDesignersTabs();
+    });
 })();
 
